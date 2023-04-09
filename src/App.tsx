6139c9dd01ec1b413705678941656281d16045f6
Gowart <gowart24@gmail.com>
2023-04-09 17:48:08 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import './assets/styles/index.scss';
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import MainPage from './components/MainPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import taskSlice from './slices/taskSlice';
+
+export const store = configureStore({
+  reducer: {
+    taskSlice
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
