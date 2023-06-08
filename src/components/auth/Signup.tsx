3c5b3a5d4132d742b749a02d9ec970f28d7d4980
Gowart <gowart24@gmail.com>
2023-06-08 14:27:35 +0300

    update

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -5,17 +5,33 @@ interface IProps {
   setEmail: React.Dispatch<React.SetStateAction<string>>;
   pass: string;
   setPass: React.Dispatch<React.SetStateAction<string>>;
+  doublePass: string;
+  setDoublePass: React.Dispatch<React.SetStateAction<string>>;
   name: string;
   setName: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const Signup: React.FC<IProps> = (props): JSX.Element => {
-  const { email, pass, setEmail, setPass, name, setName } = props;
+  const {
+    email,
+    pass,
+    setEmail,
+    setPass,
+    name,
+    setName,
+    doublePass,
+    setDoublePass,
+  } = props;
   return (
     <div>
       <Input placeholder="Введите имя" value={name} onChange={setName} />
       <Input placeholder="Введите email" value={email} onChange={setEmail} />
       <Input placeholder="Введите пароль" value={pass} onChange={setPass} />
+      <Input
+        placeholder="Введите пароль еще раз"
+        value={doublePass}
+        onChange={setDoublePass}
+      />
     </div>
   );
 };
