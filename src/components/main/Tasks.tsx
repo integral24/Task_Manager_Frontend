d231577ef23ecf79273ab470ba43aa6c9995d9f9
aleksandrlubockin <lubochkin.aleksandr@zoloto585.ru>
2023-07-03 11:26:20 +0300

    task block height dinamic

--- src/components/main/Tasks.tsx
+++ src/components/main/Tasks.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
-import { memo } from 'react';
+import { log } from 'console';
+import { memo, useEffect, useRef } from 'react';
 
 import { ITask } from '@/types/TasksTypes';
 
@@ -18,9 +19,43 @@ const Tasks: React.FC<IProps> = memo(function TaskComponent({
 	editTask,
 	deleteTask,
 }): JSX.Element {
+	const taskBlockRef = useRef<HTMLDivElement>(null);
+
+	// const resizeObserver = new ResizeObserver((entries) => {
+	// 	const elem = entries[0]
+	// 	console.log(elem);
+	// });
+
+	const resizeHandler = () => {
+		const current = taskBlockRef.current;
+		if (current) {
+			const body = document.body;
+			const tb = current.getBoundingClientRect();
+
+			const runResizeHeight = (size: number) =>
+				(current.style.height = size + 'px');
+
+			if (tb.bottom > body.clientHeight) {
+				runResizeHeight(tb.height - (tb.bottom - body.clientHeight + 40));
+			} else {
+				// runResizeHeight(tb.height + (tb.bottom - body.clientHeight + 40));
+				console.log('tb.height', tb.height);
+				console.log('tb.bottom', tb.bottom);
+				console.log('body.clientHeight', body.clientHeight);
+			}
+		}
+	};
+
+	useEffect(() => {
+		resizeHandler();
+		window.addEventListener('resize', resizeHandler, true);
+
+		return () => window.removeEventListener('resize', resizeHandler);
+	}, [taskBlockRef]);
+
 	return (
 		<>
-			<div className="tasks">
+			<div ref={taskBlockRef} className="tasks">
 				{tasks.map((task) => (
 					<div key={task.id} className="task-item">
 						<div title={task.type} className="task-item__title">
